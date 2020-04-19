36310c0e2bb0275ae67e5bd4617b5c086eb04c7b
Botnet <crataristo4@gmail.com>
2020-04-19 14:07:14 -0800

    fix

--- app/src/main/java/com/artisans/qwikhomeservices/activities/home/fragments/HomeFragment.java
+++ app/src/main/java/com/artisans/qwikhomeservices/activities/home/fragments/HomeFragment.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,17 +20,25 @@ import com.artisans.qwikhomeservices.activities.home.serviceTypes.AllBarbersActi
 import com.artisans.qwikhomeservices.databinding.FragmentHomeBinding;
 import com.artisans.qwikhomeservices.utils.MyConstants;
 
-/**
- * A simple {@link Fragment} subclass.
- */
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
 public class HomeFragment extends Fragment {
     private FragmentHomeBinding fragmentHomeBinding;
     private Intent intent;
+    private ListView listView;
+    private ArrayAdapter<String> adapter;
 
     public HomeFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
@@ -42,7 +53,7 @@ public class HomeFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
 
-        fragmentHomeBinding.mMaterialCard1.setOnClickListener(v -> {
+        /*fragmentHomeBinding.mMaterialCard1.setOnClickListener(v -> {
 
             intent = new Intent(getContext(), AllBarbersActivity.class);
             intent.putExtra(MyConstants.ACCOUNT_TYPE, MyConstants.WOMEN_HAIR_STYLIST);
@@ -67,6 +78,38 @@ public class HomeFragment extends Fragment {
 
 
         });
+*/
+        listView = fragmentHomeBinding.servicesListView;
+        //string list from xml
+        List<String> serviceList = Arrays.asList(getResources().getStringArray(R.array.services));
+
+        adapter = new ArrayAdapter<>(Objects.requireNonNull(getActivity()),android.R.layout.simple_list_item_1,serviceList);
+        listView.setAdapter(adapter);
+
+        //on item click
+        listView.setOnItemClickListener((parent, view1, position, id) -> {
+            switch (position){
+                case 0:
+                    intent = new Intent(getContext(), AllBarbersActivity.class);
+                    intent.putExtra(MyConstants.ACCOUNT_TYPE, MyConstants.WOMEN_HAIR_STYLIST);
+                    startActivity(intent);
+                    break;
+                case 1:
+                    intent = new Intent(getContext(), AllBarbersActivity.class);
+                    intent.putExtra(MyConstants.ACCOUNT_TYPE, MyConstants.INTERIOR_DERCORATOR);
+                    startActivity(intent);
+                    break;
+                case 2:
+                    intent = new Intent(getContext(), AllBarbersActivity.class);
+                    intent.putExtra(MyConstants.ACCOUNT_TYPE, MyConstants.BARBERS);
+                    startActivity(intent);
+                    break;
+
+
+
+            }
+        });
+
 
     }
 }
